2968835510ebdec7cd6f3435f6c84ebf4fd5dc13
mariasazhina <msazhina23@gmail.com>
2023-12-29 01:11:02 +0300

    add tests & poetry files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,7 @@ def test_load_operations():
     TEST_DATA_PATH = os.path.join(ROOT_DIR, 'tests', 'data_for_test.json')
     assert utils.load_operations(TEST_DATA_PATH) == [1, 2, 3]
 
+
 def test_executed_operations():
     executed_operations = [
   {
@@ -95,5 +96,3 @@ def test_formatted_operations():
     "to": "Счет 64686473678894779589"
   }]
   assert utils.formatted_operations(for_format) == "26.08.2019 Перевод организации\nMaestro 1596 83** **** 5199 -> Счет **9589\n31957.58 руб.\n"
-
-
